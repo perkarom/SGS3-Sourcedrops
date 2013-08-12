c175d7eb0d5fe600153d1dc6596748657c2cc909
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2013-08-12 05:49:37 +0200

    Linux 3.0.90
    Conflicts:
    
    	arch/arm/configs/m0_00_defconfig
    
    Conflicts:
    
    	arch/arm/configs/m0_00_defconfig

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-export CROSS_COMPILE=/home/perka/kernel/toolchains/arm-unknown-linux-gnueabi-4.7.4/bin/arm-gnueabi-
+export CROSS_COMPILE=/home/perka/kernel/toolchains/arm-eabi-4.4.3/bin/arm-eabi-
 make mrproper
-make shark-e_defconfig
+make m0_00_defconfig
 time make -j3
 echo ""
 echo "Done"
